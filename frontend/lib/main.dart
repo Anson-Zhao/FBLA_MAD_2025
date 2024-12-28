680dc61d99897296991be7c6e27e9a16349d7e9f
Northernstud <ngocbach.giap@g.northernacademy.org>
2024-12-27 20:14:06 -0600

    modified main.dart

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:edu_venture/flashcards/flashcard.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_localizations/flutter_localizations.dart';
