31fc6f28d8e9e44e517d67b7438d460198380093
imacbig08 <ngocbach.giap@g.northernacademy.org>
2025-03-01 20:55:24 -0500

    clear the main branch

--- backend/utils/validators.js
+++
@@ -1,6 +0,0 @@
-const isValidPassword = (password) => {
-    const regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-    return regex.test(password);
-};
-
-module.exports = { isValidPassword };
--- frontend/README.md
+++
@@ -1,7 +0,0 @@
-# EduVenture
-
-A new Flutter project.
-
-## Getting Started
-
-FlutterFlow projects are built to run on the Flutter _stable_ release.
